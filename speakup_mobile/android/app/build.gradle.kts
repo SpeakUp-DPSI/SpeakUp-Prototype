7acfbfa94419262a3f36e9f88a99d97a30120c39
Alief <alikhsanalief@gmail.com>
2026-07-13 05:18:34 +0700

    fix: correct desugaring config and JVM target in build.gradle.kts

--- speakup_mobile/android/app/build.gradle.kts
+++ speakup_mobile/android/app/build.gradle.kts
@@ -11,9 +11,13 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        coreLibraryDesugaringEnabled = true
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
     }
 
     defaultConfig {
@@ -41,5 +45,5 @@ flutter {
 }
 
 dependencies {
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
